4a4b7037f52b4d08b8de7a7d3341e3a0fab6940b
Soham Sajekar <sdsajekar15@gmail.com>
2022-12-30 23:25:27 -0500

    created single file for sending requests
    deleted the old

--- requests/get_entities_requests.py
+++
@@ -1,9 +0,0 @@
-import requests
-from ..utils.util import read_convert_to_jsonl
-
-json_file_path = 'resume_labels.json'
-data = read_convert_to_jsonl(json_file_path)
-service1_url = 'http://localhost:8000/get_entities'
-for payload in data:
-    service1_res = requests.get(service1_url, json=payload)
-    print(service1_res.json())
---
+++ requests/send_requests.py
@@ -0,0 +1,35 @@
+import requests
+from ..utils.util import read_convert_to_jsonl
+
+
+def send_request(json_file_path, url, service='transform_json_to_csv'):
+
+    if service == 'transform_json_to_csv':
+        # Send GET request to FastAPI
+        service2_res = requests.get(url + '/' + json_file_path)
+        # result
+        print(service2_res.json())
+    else:
+        # Read json file and convert to json-line format
+        data = read_convert_to_jsonl(json_file_path)
+        # Send GET request to FastAPI for each json line
+        for payload in data:
+            service1_res = requests.get(url, json=payload)
+            print(service1_res.json())
+
+def main():
+    # Json file path location
+    json_file_path = 'resume_labels.json'
+    # URL for service 1: get_entities
+    service1_url = 'http://localhost:8000/get_entities'
+    # URL for service 2: transform json to csv
+    service2_url = 'http://localhost:8000/transform_json_to_csv'
+    print("Service 1 - get_entities")
+    send_request(json_file_path, service1_url, service='get_entities')
+    print()
+    print("Service 2 - get_entities")
+    send_request(json_file_path, service1_url)
+
+
+if __name__ == '__main__':
+    main()
